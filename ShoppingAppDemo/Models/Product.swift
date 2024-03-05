9cacd76e3ebacdb06845a07c78b51b8d7c39a1be
shreyansh232 <shreyansh487@gmail.com>
2024-03-05 20:22:09 +0530

    Assets

--- ShoppingAppDemo/Models/Product.swift
+++ ShoppingAppDemo/Models/Product.swift
@@ -14,12 +14,13 @@ struct Product: Identifiable{
     var price: Int
 }
 var productList = [Product(name: "5 Pack Sweater", image: "sweater1", price: 399),
-                   Product(name: "Off-White Sweater", image: "sweater2", price: 59),
-                   Product(name: "Lavendar Sweater", image: "sweater3", price: 99),
                    Product(name: "Mint Sweater", image: "sweater4", price: 69),
                    Product(name: "Navy Blue Sweater", image: "sweater5", price: 59),
                    Product(name: "Yellow Blue Sweater", image: "sweater6", price: 99),
-                   Product(name: "Pink Sweater", image: "sweater7", price: 89),
-                   Product(name: "White Sweater", image: "sweater8", price: 59)]
+                   Product(name: "White Sweater", image: "sweater8", price: 59),
+                   Product(name: "Off-White Sweater", image: "sweater2", price: 59),
+                   Product(name: "Pink Sweater", image: "sweater9", price: 89),
+                   Product(name: "Lavendar Sweater", image: "sweater10", price: 99)
+                   ]
 
 
--- ShoppingAppDemo/Views/ContentView.swift
+++ ShoppingAppDemo/Views/ContentView.swift
@@ -11,9 +11,10 @@ struct ContentView: View {
     @StateObject var cartManager = CartManager()
     var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
     var body: some View {
-  
+        
            
         NavigationView {
+      
             ScrollView {
                 LazyVGrid(columns: columns, spacing: 20){
                         ForEach(productList, id: \.id) { product in
